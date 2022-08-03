2a1df326b585db50149db5e8d9ade2d952258a96
allbravoshult <110513130+allbravoshult@users.noreply.github.com>
2022-08-03 00:15:10 -0300

    Add files via upload

---
+++ config_twitter.py
@@ -0,0 +1,10 @@
+## Twitter API credentials.
+
+# Consumer keys (API key and secret)
+consumer_key = '7mTB8lV2jrH1rIN6luPkC75qI'
+consumer_secret = 'JP7L6agTM4v8haalPSpWWLKIjRFhNhii9pWj2C1IUKfDQ4VMqD'
+
+# Application access token key and access token secret
+access_token = '19968854-9fAPOfq4qbnz26JnmM51tlfbTVefwIglOk8Nv87SY'
+access_token_secret = 'jiPIQlLrUETqZGAbYw9Fmk31B9KbACrXFHe60yH3pFdeR'
+
